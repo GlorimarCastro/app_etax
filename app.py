0240b2f2b485f02ed1a1283b1d2bac2e680a4532
glorimar <glorimar@textualmind.com>
2018-01-16 22:26:57 -0400

    example changes

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ Created on Jan 16, 2018
 
 from flask import Flask
 from flask import render_template
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+
 app = Flask(__name__)
 
 
